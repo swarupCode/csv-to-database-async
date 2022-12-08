d168559d787de79a8451c36e8a7b5e44a6f9d3c9
swarupCode <43297655+swarupCode@users.noreply.github.com>
2022-12-08 22:46:28 +0530

    first commit

---
+++ src/main/java/com/swarudas/csvtodatabase/CsvToDatabaseAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.swarudas.csvtodatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToDatabaseAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToDatabaseAsyncApplication.class, args);
+	}
+
+}
